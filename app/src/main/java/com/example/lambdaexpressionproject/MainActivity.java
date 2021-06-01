8fb1301a98d2402312448344b4f87a331aad726d
mohammad <m.zandieh1999@gmail.com>
2021-06-01 14:53:41 +0430

    subjectObservable

--- app/src/main/java/com/example/lambdaexpressionproject/MainActivity.java
+++ app/src/main/java/com/example/lambdaexpressionproject/MainActivity.java
@@ -2,102 +2,36 @@ package com.example.lambdaexpressionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//1.1
-        ExampleWithoutParameter exampleWithoutParameterOld = new ExampleWithoutParameter() {
-            @Override
-            public void myFunction() {
-                System.out.println("sdfsd");
-                System.out.println("sdfsd");
-                System.out.println("sdfsd");
-                System.out.println("sdfsd");
-                System.out.println("sdfsd");
-                Log.d("dsdsd", "myFunction : ");
-            }
-        };
-//1.2
-        ExampleWithoutParameter exampleWithoutParameter = () -> {
-            System.out.println("asdadf");
-            System.out.println("asdadf");
-            System.out.println("asdadf");
-            System.out.println("asdadf");
+        //observable
+        ReplaySubject<Integer> subject = ReplaySubject.create();
+        subject.onNext(1);
+        subject.onNext(2);
+        subject.onNext(3);
 
-            Log.i("sdasd", "dsadad: ");
+        //observer
+        subject.subscribe(number -> Log.i(TAG, "Number is :" + number));
 
-        };
-//2.1
-        ExampleWithParameter exampleWithParameterOld = new ExampleWithParameter() {
-            @Override
-            public void myFunction(int x) {
-
-            }
-        };
-//2.2
-        ExampleWithParameter exampleWithParameter = (x) -> System.out.println(String.valueOf(x));
-
-//3.1
-        ExampleWithReturnType exampleWithReturnTypeOld = new ExampleWithReturnType() {
-            @Override
-            public int myFunction(int x, int y) {
-                return x * y;
-            }
-        };
-
-//3.2
-        ExampleWithReturnType exampleWithReturnType = (x, y) -> {
-            System.out.println("dfdasf");
-            System.out.println("dfdasf");
-            System.out.println("dfdasf");
-            System.out.println("dfdasf");
-
-            return x * y;
-        };
-
-//4.1
-        Button button = new Button(this);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-
-//4.2
-        Button button2 = new Button(this);
-        button2.setOnClickListener(v -> {
-
-        });
-
-
-
+        subject.onNext(4);
+        subject.onNext(5);
+        subject.onNext(6);
     }
-
-
-//1
-interface ExampleWithoutParameter {
-    void myFunction();
-}
-
-//2
-interface ExampleWithParameter {
-    void myFunction(int x);
-}
-
-//3
-interface ExampleWithReturnType {
-    int myFunction(int x, int y);
-}
 
 }
